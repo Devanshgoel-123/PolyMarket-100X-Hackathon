bedf7424fbdc7fd4a4e406b6df1c0dd05aef0161
Devansh <f20220457@pilani.bits-pilani.ac.in>
2024-09-03 21:19:13 +0530

    Finally solved the error

--- programs/polymarket/src/instructions/create_bet.rs
+++ programs/polymarket/src/instructions/create_bet.rs
@@ -1,8 +1,6 @@
 use crate::state::*;
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint, TokenAccount};
-use anchor_spl::token_interface;
-use anchor_spl::*;
+use anchor_spl::{associated_token::AssociatedToken, token_interface};
 use chrono::prelude::*;
 use std::mem::size_of;
 pub fn create_bet(
@@ -60,6 +58,7 @@ pub struct InitializeBet<'info> {
     pub token_mint: InterfaceAccount<'info, token_interface::Mint>,
     #[account(mut)]
     pub payer: Signer<'info>,
-    pub token_program: Program<'info, token_interface::TokenInterface>,
+    pub token_program: Interface<'info, token_interface::TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
     pub system_program: Program<'info, System>,
 }
